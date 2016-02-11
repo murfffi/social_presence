e72573220fce7285b9a4b7949e855f4ebd63efb1
Evelina Nozhcheva <evelina.nozhcheva@gmail.com>
2016-02-11 09:41:36 +0200

    Switch to single page edit and add

--- community/community/Global.asax.cs
+++ community/community/Global.asax.cs
@@ -41,26 +41,28 @@ namespace community
             // The following statement supports separate-page mode, where the List, Detail, Insert, and 
             // Update tasks are performed by using separate pages. To enable this mode, uncomment the following 
             // route definition, and comment out the route definitions in the combined-page mode section that follows.
-            routes.Add(new DynamicDataRoute("{table}/{action}.aspx")
-            {
-                Constraints = new RouteValueDictionary(new { action = "List|Details|Edit|Insert" }),
-                Model = DefaultModel
-            });
+            //routes.Add(new DynamicDataRoute("{table}/{action}.aspx")
+            //{
+            //    Constraints = new RouteValueDictionary(new { action = "List|Details|Edit|Insert" }),
+            //    Model = DefaultModel
+            //});
 
             // The following statements support combined-page mode, where the List, Detail, Insert, and
             // Update tasks are performed by using the same page. To enable this mode, uncomment the
             // following routes and comment out the route definition in the separate-page mode section above.
-            //routes.Add(new DynamicDataRoute("{table}/ListDetails.aspx") {
-            //    Action = PageAction.List,
-            //    ViewName = "ListDetails",
-            //    Model = DefaultModel
-            //});
+            routes.Add(new DynamicDataRoute("{table}/ListDetails.aspx")
+            {
+                Action = PageAction.List,
+                ViewName = "ListDetails",
+                Model = DefaultModel
+            });
 
-            //routes.Add(new DynamicDataRoute("{table}/ListDetails.aspx") {
-            //    Action = PageAction.Details,
-            //    ViewName = "ListDetails",
-            //    Model = DefaultModel
-            //});
+            routes.Add(new DynamicDataRoute("{table}/ListDetails.aspx")
+            {
+                Action = PageAction.Details,
+                ViewName = "ListDetails",
+                Model = DefaultModel
+            });
         }
 
         private static void RegisterScripts()
